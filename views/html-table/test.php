e95097baae5cc1826e569fd9f26d343021ee7b14
sharils <sharils@ncut-book-store>
2013-07-31 22:06:17 +0800

    fixup! add html table

---
+++ views/html-table/test.php
@@ -0,0 +1,23 @@
+<?php
+require 'HtmlTable.php';
+
+$table = new HtmlTable(12, 4, function ($row, $col) {
+  	if ($row === 0) {
+  		return true;
+  	}
+  	if ($col === 0) {
+  	 	return true;
+ 	}
+	return false;
+}, function ($row) {
+    if ($row === 0) {
+    	return 0;
+    }
+    if ($row === 1) {
+    	return 1;
+    }
+    return floor(($row - 2) / 5) + 2;
+}, function ($row, $col) {
+    return $row * 100 + $col;
+});
+echo $table;
