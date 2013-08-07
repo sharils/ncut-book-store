e64cb4ff8d569e45f28610c38305d106e82c107f
sharils <sharils@ncut-book-store>
2013-08-07 08:04:29 +0800

    add password

--- models/user/Password.php
+++ models/user/Password.php
@@ -1,13 +1,41 @@
 <?php
 class Password
 {
-    public static function from()
-    {
-        return new self();
-    }
+    private $hashed;
+    private $salt;
 
+    public static function create($pwd)
+    {
+        $salt = substr(md5(mt_rand()), 0, 20);
+        $hashed = crypt($pwd, $salt);
+        $password = self::from($hashed, $salt);
+        return $password;
+    }
+    public static function from($hashed, $salt)
+    {
+        $password = new self();
+        $password->hashed = $hashed;
+        $password->salt = $salt;
+        return $password;
+    }
+    public function password()
+    {
+        return $this->hashed;
+    }
+    public function salt()
+    {
+        return $this->salt;
+    }
     public function verify($password)
     {
-		return $password === '0000';
-	}
+        $old_password = $this->password();
+        $salt = $this->salt();
+        $new_password = crypt($password, $salt);
+        If ($old_password === $new_password) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
