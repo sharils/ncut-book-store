93efb5689b5959eba27d7040f97298119cbc5214
hsy0130 <hsy0130@ncut-book-store>
2013-08-11 09:53:26 +0800

    Refactor password

--- models/user/Password.php
+++ models/user/Password.php
@@ -11,6 +11,7 @@ class Password
         $password = self::from($hashed, $salt);
         return $password;
     }
+
     public static function from($hashed, $salt)
     {
         $password = new self();
@@ -18,24 +19,23 @@ class Password
         $password->salt = $salt;
         return $password;
     }
+
     public function password()
     {
         return $this->hashed;
     }
+
     public function salt()
     {
         return $this->salt;
     }
+
     public function verify($password)
     {
         $old_password = $this->password();
         $salt = $this->salt();
         $new_password = crypt($password, $salt);
-        If ($old_password === $new_password) {
-            return true;
-        } else {
-            return false;
-        }
+
+        return $old_password === $new_password;
     }
 }
-
