63a0437e51c5e63d8cabfd79f99dd71f62d4cc63
hsy0130 <hsy0130@ncut-book-store>
2013-08-18 14:58:26 +0800

    Add password_update views

---
+++ views/password_update.php
@@ -0,0 +1,7 @@
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
+<form action="test.php" method="post">
+	舊密碼<input name="password" type="password" />
+	新密碼<input name="newpassword" type="password" />
+	確認密碼<input name="confirmpassword" type="password" />
+	<input type="submit" value="送出"/>
+</form>
