2d3a9d072384467463891214bba59dbb45eadd8e
hsy0130 <hsy0130@ncut-book-store>
2013-08-18 16:58:02 +0800

    Update wrongword

--- controllers/password_update.php
+++ controllers/password_update.php
@@ -8,7 +8,7 @@ Database::initialise('localhost', 'root', '123456', 'ncut');
 $pwd = $_POST;
 try{
 	if ($pwd['newpassword'] === $pwd['confirmpassword']){
-		$user = User::form($_SESSION['User_id']);
+		$user = User::from($_SESSION['User_id']);
 		$user->changePassword($pwd['password'], $pwd['newpassword']);
 	}else{
 		echo '密碼輸入錯誤';
