e871d949b2aaefaefe971d4b06c1d469956567c4
hsy0130 <hsy0130@ncut-book-store>
2013-08-18 16:33:29 +0800

    Update password_update controllers

--- controllers/password_update.php
+++ controllers/password_update.php
@@ -1,12 +1,18 @@
 <?php
+require_once "Database.php";
 require_once "User.php";
+
 session_start();
 Database::initialise('localhost', 'root', '123456', 'ncut');
 
 $pwd = $_POST;
-User::changePassword($pwd['password'], $pwd['newpassword']);
-if ($pwd['newpassword'] === $pwd['confirmpassword']){
-	echo '密碼更新成功';
-}else{
-	echo '密碼輸入錯誤';
+try{
+	if ($pwd['newpassword'] === $pwd['confirmpassword']){
+		$user = User::form($_SESSION['User_id']);
+		$user->changePassword($pwd['password'], $pwd['newpassword']);
+	}else{
+		echo '密碼輸入錯誤';
+	}
+}catch (Exception $e) {
+	echo $e->getMessage();
 }
