2301d6946fe50f700fbe8b6225fed94e4f700c76
esbb48 <esbb48@ncut-book-store>
2013-08-21 22:38:27 +0800

    Add view for select method

---
+++ views/method.php
@@ -0,0 +1,12 @@
+<?php
+class Method
+{
+	public static function select($name, $args)
+	{?>
+		<select name="<?= $name ?>">
+		<?php foreach($args as $key => $value) :?>
+			<option value="<?= $key ?>"><?= $value ?></option>
+		<?php endforeach ;?>     
+		</select>
+	<?}
+}
