dfa547e19bbf6a8a2310d151007b990ae2694833
esbb48 <esbb48@ncut-book-store>
2013-08-22 19:20:10 +0800

    Fix course_creation view

--- views/course_creation.php
+++ views/course_creation.php
@@ -1,13 +1,15 @@
 <?php 
-require_once 'method.php';
-$a = array('0000000000' => 't001'.'Sam', '2221888777' => 't003'.'Fan', '1377007736' => 't00123'.'Harry'); 
+require_once 'Method.php';
+require_once '../controllers/select_list.php'; 
 ?>
-<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
 <form action="../controllers/course_creation.php" method="post">
-	<label>老師</label><?php Method::select('teacher_id',$a); ?>
-	<label>課程代碼<input name="sn" type="text" /></label>
-	<label>課程名稱<input name="name" type="text" /></label>
-	<label>必選修<input name="type" type="text" /></label>
-	<label>學年度<input name="year" type="text" /></label>
-	<input type="submit" value="送出"/>
+	<fieldset>
+		<label>老師<?php Method::select('teacher_id',$args); ?></label>
+		<label>課程代碼<input name="sn" type="text" /></label>
+		<label>課程名稱<input name="name" type="text" /></label>
+		<label>必選修<input name="type" type="text" /></label>
+		<label>學年度<input name="year" type="text" /></label>
+		<input type="submit" value="送出"/>
+	</fieldset>
 </form>
