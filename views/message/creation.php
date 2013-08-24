f19652dc76fe34980c2c8294f542b7770bac6a0e
esbb48 <esbb48@ncut-book-store>
2013-08-24 17:18:24 +0800

    Add views/message/

---
+++ views/message/creation.php
@@ -0,0 +1,9 @@
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+<form action="../../controllers/message/creation.php" method="post">
+	<fieldset>
+		<label>收訊人：<input name="receiver" type="text"/></label>
+		<label>內容：<textarea cols="30" rows="4" name="content"></textarea></label>
+		<input type="submit" value="送出"/>
+	</fieldset>
+</form>
