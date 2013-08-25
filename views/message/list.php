8053315a6d0aedd7b951d2effa6d90453c4fe00e
esbb48 <esbb48@ncut-book-store>
2013-08-25 17:40:56 +0800

    Add list view for message

---
+++ views/message/list.php
@@ -0,0 +1,25 @@
+<?php require_once '../../controllers/message/listing.php'; ?>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<a href="list.php?page=spam">[垃圾筒]</a>
+<a href="list.php?page=receive">[收件匣]</a>
+<a href="list.php?page=send">[寄件匣]</a>
+<table>
+	<caption>訊息列表</caption>
+	<tr>
+		<th>寄件人</th>
+		<th>內容</th>
+		<th>時間</th>
+		<th>功能</th>
+	</tr>
+	<?php foreach($messageslist as $message): ?>
+		<tr>
+			<td><?= $message->sender()->toRole()->sn()?></td>
+			<td><?= $message->content() ?></td>
+			<td><?= $message->date() ?></td>
+			<td>
+				<a href="detail.php?id=<?= $message->id() ?>">查閱</a>
+				<a href="../../controllers/message/delete.php?id=<?= $message->id() ?>">刪除</a>
+			</td>
+		</tr>
+	<?php endforeach; ?>
+</table>
