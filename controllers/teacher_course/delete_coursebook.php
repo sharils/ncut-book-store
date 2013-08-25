0f4a26e16dd7800e953adc4061c14e3ac3486dcc
hsy0130 <hsy0130@ncut-book-store>
2013-08-25 18:05:43 +0800

    Add coursebook delete handler

---
+++ controllers/teacher_course/delete_coursebook.php
@@ -0,0 +1,15 @@
+<?php
+require_once '../models/database/Database.php';
+require_once '../models/course/Course.php';
+require_once '../models/coursebook/CourseBook.php';
+require_once '../models/teacher/Teacher.php';
+require_once '../models/user/User.php';
+require_once '../models/book/Book.php';
+Database::initialise('localhost', 'root', '123456', 'ncut');
+$get = $_GET;
+if (in_array('', $get)){
+	echo 'Have null value';
+} else {
+	$coursebook = CourseBook::from($get['book-id']);
+	$delete = $coursebook->delete();
+}
