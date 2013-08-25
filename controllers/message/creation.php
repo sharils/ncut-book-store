27936a99d1637c607e3ff969de3c884ccdbd7a16
esbb48 <esbb48@ncut-book-store>
2013-08-25 17:34:17 +0800

    Update message creation controller

--- controllers/message/creation.php
+++ controllers/message/creation.php
@@ -1,10 +1,9 @@
 <?php
-require_once '../../models/Database.php';
-require_once '../../models/Message.php';
-require_once '../../models/User.php';
+require_once '../../models/database/Database.php';
+require_once '../../models/message/Message.php';
+require_once '../../models/user/User.php';
 session_start();
 Database::initialise('localhost', 'root', '123456', 'ncut');
-// $Sender = User::from($_SESSION['user_id']);
-$Sender = User::from('3273891391');
+$Sender = User::from($_SESSION['user_id']);
 $Receiver = User::find($_POST['receiver']);
-Message::create($Sender, $Receiver, $_POST['content']);
+$m = Message::create($Sender, $Receiver, $_POST['content']);
