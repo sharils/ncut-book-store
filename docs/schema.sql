56c6b06cd61eca2e9e90d8aa0c13c5fd5144edd0
esbb48 <esbb48@ncut-book-store>
2013-08-29 19:51:18 +0800

    Add shelf field for shopbook table

--- docs/schema.sql
+++ docs/schema.sql
@@ -20,15 +20,15 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- 
 
 CREATE TABLE `admin` (
-  `user_id` char(10) collate utf8_bin NOT NULL,
-  `sn` char(50) collate utf8_bin NOT NULL,
-  `email` varchar(50) collate utf8_bin NOT NULL,
+  `user_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `sn` char(50) collate utf8_unicode_ci NOT NULL,
+  `email` varchar(50) collate utf8_unicode_ci NOT NULL,
   `name` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
-  `phone` char(10) collate utf8_bin NOT NULL,
-  `phone_ext` char(5) collate utf8_bin NOT NULL,
+  `phone` char(10) collate utf8_unicode_ci NOT NULL,
+  `phone_ext` char(5) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`user_id`),
   UNIQUE KEY `sn` (`sn`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `admin`
@@ -43,11 +43,11 @@ INSERT INTO `admin` VALUES (0x33323733383931333931, 0x61646d696e, 0x61646d696e40
 -- 
 
 CREATE TABLE `blacklist` (
-  `user_id` char(10) collate utf8_bin NOT NULL,
-  `black_user_id` char(10) collate utf8_bin NOT NULL,
+  `user_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `black_user_id` char(10) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`user_id`,`black_user_id`),
   KEY `black_user_id` (`black_user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `blacklist`
@@ -61,20 +61,20 @@ CREATE TABLE `blacklist` (
 -- 
 
 CREATE TABLE `book` (
-  `id` char(10) collate utf8_bin NOT NULL,
-  `publisher_id` char(10) collate utf8_bin NOT NULL,
-  `author` varchar(50) collate utf8_bin NOT NULL,
-  `isbn` char(15) collate utf8_bin NOT NULL,
+  `id` char(10) collate utf8_unicode_ci NOT NULL,
+  `publisher_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `author` varchar(50) collate utf8_unicode_ci NOT NULL,
+  `isbn` char(15) collate utf8_unicode_ci NOT NULL,
   `market_price` int(4) NOT NULL,
   `name` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
   `price` int(4) NOT NULL,
   `remark` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
-  `type` char(50) collate utf8_bin NOT NULL,
-  `version` char(10) collate utf8_bin NOT NULL,
+  `type` char(50) collate utf8_unicode_ci NOT NULL,
+  `version` char(10) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `isbn` (`isbn`),
   KEY `publisher_id` (`publisher_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `book`
@@ -88,15 +88,15 @@ CREATE TABLE `book` (
 -- 
 
 CREATE TABLE `clerk` (
-  `user_id` char(10) collate utf8_bin NOT NULL,
-  `sn` char(50) collate utf8_bin NOT NULL,
-  `email` varchar(50) collate utf8_bin NOT NULL,
+  `user_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `sn` char(50) collate utf8_unicode_ci NOT NULL,
+  `email` varchar(50) collate utf8_unicode_ci NOT NULL,
   `name` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
-  `phone` char(10) collate utf8_bin NOT NULL,
-  `phone_ext` char(5) collate utf8_bin NOT NULL,
+  `phone` char(10) collate utf8_unicode_ci NOT NULL,
+  `phone_ext` char(5) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`user_id`),
   UNIQUE KEY `sn` (`sn`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=FIXED;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=FIXED;
 
 -- 
 -- 列出以下資料庫的數據： `clerk`
@@ -110,15 +110,15 @@ CREATE TABLE `clerk` (
 -- 
 
 CREATE TABLE `course` (
-  `id` char(10) collate utf8_bin NOT NULL,
-  `sn` char(10) collate utf8_bin NOT NULL,
-  `teacher_user_id` char(10) collate utf8_bin NOT NULL,
-  `type` enum('required','optional') collate utf8_bin NOT NULL,
+  `id` char(10) collate utf8_unicode_ci NOT NULL,
+  `sn` char(10) collate utf8_unicode_ci NOT NULL,
+  `teacher_user_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `type` enum('required','optional') collate utf8_unicode_ci NOT NULL,
   `name` char(50) character set utf8 collate utf8_unicode_ci NOT NULL,
   `year` int(4) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `teacher_user_id` (`teacher_user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `course`
@@ -132,11 +132,11 @@ CREATE TABLE `course` (
 -- 
 
 CREATE TABLE `course_book` (
-  `course_id` char(10) collate utf8_bin NOT NULL,
-  `book_id` char(10) collate utf8_bin NOT NULL,
-  `sample` enum('','1') collate utf8_bin NOT NULL,
+  `course_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `book_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `sample` enum('','1') collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`course_id`,`book_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `course_book`
@@ -150,15 +150,15 @@ CREATE TABLE `course_book` (
 -- 
 
 CREATE TABLE `message` (
-  `id` char(10) collate utf8_bin NOT NULL,
-  `sender_user_id` char(10) collate utf8_bin NOT NULL,
-  `receiver_user_id` char(10) collate utf8_bin NOT NULL,
+  `id` char(10) collate utf8_unicode_ci NOT NULL,
+  `sender_user_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `receiver_user_id` char(10) collate utf8_unicode_ci NOT NULL,
   `date` datetime NOT NULL,
-  `content` text collate utf8_bin NOT NULL,
+  `content` text collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `sender_user_id` (`sender_user_id`),
   KEY `receiver_user_id` (`receiver_user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `message`
@@ -172,15 +172,15 @@ CREATE TABLE `message` (
 -- 
 
 CREATE TABLE `publisher` (
-  `id` char(10) collate utf8_bin NOT NULL default '0',
-  `email` varchar(50) collate utf8_bin NOT NULL,
-  `account` char(50) collate utf8_bin NOT NULL,
-  `address` varchar(50) collate utf8_bin NOT NULL,
+  `id` char(10) collate utf8_unicode_ci NOT NULL default '0',
+  `email` varchar(50) collate utf8_unicode_ci NOT NULL,
+  `account` char(50) collate utf8_unicode_ci NOT NULL,
+  `address` varchar(50) collate utf8_unicode_ci NOT NULL,
   `name` char(50) character set utf8 collate utf8_unicode_ci NOT NULL,
   `person` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
-  `phone` char(10) collate utf8_bin NOT NULL,
+  `phone` char(10) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `publisher`
@@ -194,10 +194,11 @@ CREATE TABLE `publisher` (
 -- 
 
 CREATE TABLE `shop_book` (
-  `book_id` char(10) collate utf8_bin NOT NULL,
+  `book_id` char(10) collate utf8_unicode_ci NOT NULL,
   `num` int(4) NOT NULL,
+  `shelf` enum('','1') collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`book_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `shop_book`
@@ -211,11 +212,11 @@ CREATE TABLE `shop_book` (
 -- 
 
 CREATE TABLE `shop_order` (
-  `id` char(10) collate utf8_bin NOT NULL,
-  `clerk_user_id` char(10) collate utf8_bin NOT NULL,
+  `id` char(10) collate utf8_unicode_ci NOT NULL,
+  `clerk_user_id` char(10) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`clerk_user_id`),
   KEY `clerk_user_id` (`clerk_user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `shop_order`
@@ -229,13 +230,13 @@ CREATE TABLE `shop_order` (
 -- 
 
 CREATE TABLE `shop_order_detail` (
-  `id` char(10) collate utf8_bin NOT NULL,
-  `shop_order_id` char(10) collate utf8_bin NOT NULL,
-  `book_id` char(10) collate utf8_bin NOT NULL,
+  `id` char(10) collate utf8_unicode_ci NOT NULL,
+  `shop_order_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `book_id` char(10) collate utf8_unicode_ci NOT NULL,
   `num` int(4) NOT NULL,
   PRIMARY KEY  (`shop_order_id`),
   KEY `book_id` (`book_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=FIXED;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=FIXED;
 
 -- 
 -- 列出以下資料庫的數據： `shop_order_detail`
@@ -249,17 +250,17 @@ CREATE TABLE `shop_order_detail` (
 -- 
 
 CREATE TABLE `student` (
-  `user_id` char(10) collate utf8_bin NOT NULL,
-  `sn` char(8) collate utf8_bin NOT NULL,
-  `email` varchar(50) collate utf8_bin NOT NULL,
-  `class` enum('') collate utf8_bin NOT NULL,
-  `department` varchar(50) collate utf8_bin NOT NULL,
+  `user_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `sn` char(8) collate utf8_unicode_ci NOT NULL,
+  `email` varchar(50) collate utf8_unicode_ci NOT NULL,
+  `class` enum('') collate utf8_unicode_ci NOT NULL,
+  `department` varchar(50) collate utf8_unicode_ci NOT NULL,
   `name` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
-  `type` enum('') collate utf8_bin NOT NULL,
-  `phone` char(10) collate utf8_bin NOT NULL,
+  `type` enum('') collate utf8_unicode_ci NOT NULL,
+  `phone` char(10) collate utf8_unicode_ci NOT NULL,
   `year` int(4) NOT NULL,
   PRIMARY KEY  (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `student`
@@ -273,11 +274,11 @@ CREATE TABLE `student` (
 -- 
 
 CREATE TABLE `student_course` (
-  `student_user_id` char(10) collate utf8_bin NOT NULL,
-  `course_id` char(10) collate utf8_bin NOT NULL,
+  `student_user_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `course_id` char(10) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`student_user_id`),
   KEY `course_id` (`course_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=FIXED;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=FIXED;
 
 -- 
 -- 列出以下資料庫的數據： `student_course`
@@ -291,16 +292,16 @@ CREATE TABLE `student_course` (
 -- 
 
 CREATE TABLE `student_order` (
-  `id` char(10) collate utf8_bin NOT NULL,
-  `student_user_id` char(10) collate utf8_bin NOT NULL,
-  `clerk_user_id` char(10) collate utf8_bin NOT NULL,
+  `id` char(10) collate utf8_unicode_ci NOT NULL,
+  `student_user_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `clerk_user_id` char(10) collate utf8_unicode_ci NOT NULL,
   `date` datetime NOT NULL,
   `outdate` int(10) NOT NULL,
-  `status` enum('shopping','submitted','processing','ordered','shipping','arrived') collate utf8_bin NOT NULL,
+  `status` enum('shopping','submitted','processing','ordered','shipping','arrived') collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `student_user_id` (`student_user_id`),
   KEY `clerk_user_id` (`clerk_user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `student_order`
@@ -314,13 +315,13 @@ CREATE TABLE `student_order` (
 -- 
 
 CREATE TABLE `student_order_detail` (
-  `id` char(10) collate utf8_bin NOT NULL,
-  `student_order_id` char(10) collate utf8_bin NOT NULL,
-  `book_id` char(10) collate utf8_bin NOT NULL,
+  `id` char(10) collate utf8_unicode_ci NOT NULL,
+  `student_order_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `book_id` char(10) collate utf8_unicode_ci NOT NULL,
   `num` int(4) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `book_id` (`book_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=FIXED;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=FIXED;
 
 -- 
 -- 列出以下資料庫的數據： `student_order_detail`
@@ -334,15 +335,15 @@ CREATE TABLE `student_order_detail` (
 -- 
 
 CREATE TABLE `teacher` (
-  `user_id` char(10) collate utf8_bin NOT NULL,
-  `sn` varchar(50) collate utf8_bin NOT NULL,
-  `email` varchar(50) collate utf8_bin NOT NULL,
+  `user_id` char(10) collate utf8_unicode_ci NOT NULL,
+  `sn` varchar(50) collate utf8_unicode_ci NOT NULL,
+  `email` varchar(50) collate utf8_unicode_ci NOT NULL,
   `name` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
-  `phone` char(10) collate utf8_bin NOT NULL,
-  `phone_ext` char(5) collate utf8_bin NOT NULL,
+  `phone` char(10) collate utf8_unicode_ci NOT NULL,
+  `phone_ext` char(5) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`user_id`),
   UNIQUE KEY `sn` (`sn`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=FIXED;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=FIXED;
 
 -- 
 -- 列出以下資料庫的數據： `teacher`
@@ -356,12 +357,12 @@ CREATE TABLE `teacher` (
 -- 
 
 CREATE TABLE `user` (
-  `id` char(10) collate utf8_bin NOT NULL,
-  `pwd` varchar(50) collate utf8_bin NOT NULL,
-  `salt` char(40) collate utf8_bin NOT NULL,
+  `id` char(10) collate utf8_unicode_ci NOT NULL,
+  `pwd` varchar(50) collate utf8_unicode_ci NOT NULL,
+  `salt` char(40) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `user_id` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 
 -- 列出以下資料庫的數據： `user`
