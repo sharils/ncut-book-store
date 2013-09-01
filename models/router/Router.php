d3ca1b2ec19f4e6e027f2ed3b43657fd9d91471d
sharils <sharils@ncut-book-store>
2013-09-01 10:22:25 +0800

    Fix typo

--- models/router/Router.php
+++ models/router/Router.php
@@ -18,7 +18,7 @@ class Router
         echo $status;
     }
 
-    public static function rediect($url)
+    public static function redirect($url)
     {
         header("HTTP/1.1 302 Found");
         header("Location: $url");
