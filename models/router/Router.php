b9fda45f4aa4b1652e5d17931bdb2bf7d9171808
sharils <sharils@ncut-book-store>
2013-09-01 10:28:01 +0800

    Reorder methods

--- models/router/Router.php
+++ models/router/Router.php
@@ -4,24 +4,28 @@ class Router
     private static $DOCUMENT_ROOT = 'ncut-book-store/';
     private static $hostName = null;
 
-    public static function hostName($protocol, $host, $port) {
-        $port = $port === 80 ? '' : ":$port";
-
-        self::$hostName = "$protocol://$host$port/";
+    public static function above()
+    {
+        ?>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+ "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>I AM YOUR DOCUMENT TITLE REPLACE ME</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Content-Style-Type" content="text/css">
+</head>
+<body>
+        <?php
     }
 
-    private static function notFound()
+    public static function below()
     {
-        $status = "404 Not Found";
-
-        header("HTTP/1.0 $status");
-        echo $status;
-    }
-
-    public static function redirect($url)
-    {
-        header("HTTP/1.1 302 Found");
-        header("Location: $url");
+        ?>
+                </body>
+            </html>
+        <?php
     }
 
     public static function route($redirect_url)
@@ -45,28 +49,20 @@ class Router
         require_once $redirect_url;
     }
 
-    public static function above()
+    private static function notFound()
     {
-        ?>
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
- "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-<title>I AM YOUR DOCUMENT TITLE REPLACE ME</title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="Content-Style-Type" content="text/css">
-</head>
-<body>
-        <?php
+        $status = "404 Not Found";
+
+        header("HTTP/1.0 $status");
+        echo $status;
     }
-    public static function below()
+
+    public static function rediect($url)
     {
-        ?>
-                </body>
-            </html>
-        <?php
+        header("HTTP/1.1 302 Found");
+        header("Location: $url");
     }
+
     public static function hostName($protocol, $host, $port) {
         $port = $port === 80 ? '' : ":$port";
 
