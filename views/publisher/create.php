f160eccb3cbfe0ce32477e0108a5a349dc1d2d62
esbb48 <esbb48@ncut-book-store>
2013-09-04 14:34:06 +0800

    Fix path

--- views/publisher/create.php
+++ views/publisher/create.php
@@ -1,4 +1,4 @@
-<form action="../../controllers/publisher/create.php" method="post">
+<form action="<?= Router::toUrl('controllers/publisher/create.php') ?>" method="post">
 	<fieldset>
 		<label>廠商名稱<input name="name" type="text"/></label>
 		<label>信箱<input name="email" type="text"/></label>
@@ -6,7 +6,6 @@
 		<label>匯款帳戶<input name="account" type="text"/></label>
 		<label>負責人<input name="person" type="text"/></label>
 		<label>連絡電話<input name="phone" type="text"/></label>
-		<input type="reset" value="返回"/>
 		<input type="submit" value="送出"/>
 	</fieldset>
 </form>
