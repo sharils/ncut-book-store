a598240a400937bdf186a5b904f2d64b9b179e44
esbb48 <esbb48@ncut-book-store>
2013-09-13 21:48:23 +0800

    Finish i70

--- controllers/student_course/list.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'models/student/Student.php';
-require_once 'models/studentcourse/StudentCourse.php';
-require_once 'models/user/User.php';
-
-$student_courses = StudentCourse::from(Student::from(User::from($_SESSION['user_id'])));
