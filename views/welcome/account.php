8a446e1b08e335fdc4d9f5d21659e79a88b45687
esbb48 <esbb48@ncut-book-store>
2013-09-13 19:03:18 +0800

    Finish i65

--- views/welcome/account.php
+++ views/welcome/account.php
@@ -1,4 +1,6 @@
 <ul class="nav">
-    <li><a href="<?= Router::toUrl('account/modify'); ?>">個人資料</a></li>
+    <?php if ($role === 'admin'): ?>
+        <li><a href="<?= Router::toUrl('account/modify'); ?>">個人資料</a></li>
+    <?php endif ?>
     <li><a href="<?= Router::toUrl('account/update'); ?>">修改密碼</a></li>
 </ul>
