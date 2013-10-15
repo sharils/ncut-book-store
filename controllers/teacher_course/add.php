3deb21e4d27d54ddaabe45c187d4060354d93e96
esbb48 <esbb48@ncut-book-store>
2013-10-15 23:03:22 +0800

    Update for teacher can see his history coursebook

--- controllers/teacher_course/add.php
+++ controllers/teacher_course/add.php
@@ -9,45 +9,66 @@ $id = Router::resource(2);
 
 //Load顯示時
 if (isset($id)) {
+    $historybooks = [];
+    $teacher_id = $_SESSION['user_id'];
+    $courses = Course::find(['teacher_user_id' => $teacher_id]);
+    foreach ($courses as $course) {
+        $course_books = CourseBook::findCourse($course);
+
+        foreach($course_books as $course_book){
+
+            $historybooks[] = $course_book;
+        }
+    }
     $course = Course::from($id);
 }
 
-//按下新增扭
-if (isset($_POST['add_book'])) {
-
-    unset($_POST['add_book']);
-    $_POST['remark'] = ' ';
-    if (in_array('', $_POST)) {
-        $url = Notice::addTo('新增失敗：不允許空值存入！',"home/course_book/new/{$_POST['course']}");
-        $redirect_url = Router::toUrl($url);
-        Router::redirect($redirect_url);
-        exit;
-    }
-
-    if (Book::findIsbn($_POST['isbn']) !== FALSE) {
-        $url = Notice::addTo('新增失敗：ISBN碼重複！',"home/course_book/new/{$_POST['course']}");
-        $redirect_url = Router::toUrl($url);
-        Router::redirect($redirect_url);
-        exit;
-    }
-
+if (isset($_POST['course'])) {
     $course = Course::from($_POST['course']);
-    $publisher = Publisher::from($_POST['publisher']);
+    $sample ='';
+
+    //按下新增扭
+    if (isset($_POST['add_book'])){
+        unset($_POST['add_book']);
+        $_POST['remark'] = ' ';
+        if (in_array('', $_POST)) {
+            $url = Notice::addTo('新增失敗：不允許空值存入！',"home/course_book/new/{$_POST['course']}");
+            $redirect_url = Router::toUrl($url);
+            Router::redirect($redirect_url);
+            exit;
+        }
+
+        if (Book::findIsbn($_POST['isbn']) !== FALSE) {
+            $book = Book::findIsbn($_POST['isbn']);
+            Coursebook::create($course, $book, $sample);
+            $url = Router::toUrl("home/course/{$_POST['course']}");
+            Router::redirect($url);
+            exit;
+        }
+
+        $publisher = Publisher::from($_POST['publisher']);
+
+        $book = Book::create(
+            $publisher,
+            $_POST['auther'],
+            $_POST['isbn'],
+            '',
+            $_POST['name'],
+            '',
+            $_POST['remark'],
+            '',
+            $_POST['version']
+        );
+
+        $sample = ($_POST['sample']) ? '1' : '';
+
+    } elseif(isset($_POST['history'])) {
+
+        $book = Book::from($_POST['history']);
+
+    }
 
 
-    $book = Book::create(
-        $publisher,
-        $_POST['auther'],
-        $_POST['isbn'],
-        '',
-        $_POST['name'],
-        '',
-        $_POST['remark'],
-        '',
-        $_POST['version']
-    );
-
-    $sample = ($_POST['sample']) ? '1' : '';
     Coursebook::create($course, $book, $sample);
     Shopbook::create($book, 0, '');
     $url = Router::toUrl("home/course/{$_POST['course']}");
--- views/teacher_course/teacher_coursedetail.php
+++ views/teacher_course/teacher_coursedetail.php
@@ -2,28 +2,28 @@
 require_once 'controllers/teacher_course/teacher_coursedetail.php';
 ?>
 <div class ="col-lg-9 col-sm-9 center">
-<form action="<?= Router::toUrl("controllers/teacher_course/remove.php")?>" method="post">
-    <div><input name="course" type="hidden" value="<?= $course->id(); ?>"/></div>
-    <table class="table table-bordered center">
-        <tr class="active">
-            <th><label>書籍名稱</label></th>
-            <th><label>版本</label></th>
-            <th><label>ISBN</label></th>
-            <th><label>作者</label></th>
-            <th><label>功能</label></th>
-        </tr>
-        <?php foreach( $coursebooks as $coursebook ): ?>
-            <tr>
-                <td><?= htmlspecialchars($coursebook->book()->name()); ?></td>
-                <td><?= htmlspecialchars($coursebook->book()->version()); ?></td>
-                <td><?= htmlspecialchars($coursebook->book()->isbn()); ?></td>
-                <td><?= htmlspecialchars($coursebook->book()->author()) ?></td>
-                <td>
-                    <button class="btn btn-danger" name="remove_book" value="<?= htmlspecialchars($coursebook->book()->id()) ?>">刪除書籍</button>
-                </td>
+    <form action="<?= Router::toUrl("controllers/teacher_course/remove.php")?>" method="post">
+        <div><input name="course" type="hidden" value="<?= $objcourse->id(); ?>"/></div>
+        <table class="table table-bordered center">
+            <tr class="active">
+                <th><label>書籍名稱</label></th>
+                <th><label>版本</label></th>
+                <th><label>ISBN</label></th>
+                <th><label>作者</label></th>
+                <th><label>功能</label></th>
             </tr>
-         <?php endforeach; ?>
-    </table>
-    <p><a class="btn btn-primary" name="add_book" href="<?= Router::toUrl("home/course_book/new/{$course->id()}")?>">新增書籍</a></p>
-</form>
+            <?php foreach( $coursebooks as $coursebook ): ?>
+                <tr>
+                    <td><?= htmlspecialchars($coursebook->book()->name()); ?></td>
+                    <td><?= htmlspecialchars($coursebook->book()->version()); ?></td>
+                    <td><?= htmlspecialchars($coursebook->book()->isbn()); ?></td>
+                    <td><?= htmlspecialchars($coursebook->book()->author()) ?></td>
+                    <td>
+                        <button class="btn btn-danger" name="remove_book" value="<?= htmlspecialchars($coursebook->book()->id()) ?>">刪除書籍</button>
+                    </td>
+                </tr>
+             <?php endforeach; ?>
+        </table>
+        <p><a class="btn btn-primary" name="add_book" href="<?= Router::toUrl("home/course_book/new/{$objcourse->id()}")?>">新增書籍</a></p>
+    </form>
 </div>
