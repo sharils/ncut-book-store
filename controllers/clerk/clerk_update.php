7c30db7954b6d1c24eb9dfcb892d509321279afc
esbb48 <4j1k3>
2013-10-18 21:38:19 +0800

    Update student order management for clerk

--- controllers/clerk/clerk_update.php
+++ controllers/clerk/clerk_update.php
@@ -1,10 +1,14 @@
 <?php
+require_once 'models/user/User.php';
 require_once 'models/studentorder/StudentOrder.php';
 require_once 'models/studentorderdetail/StudentOrderDetail.php';
 if (isset($_POST['update'])) {
+    $user = User::from($_POST['clerk_id']);
+    $clerk = $user->toRole();
     $order = studentorder::from($_POST['update']);
     $orderdetail = studentorderdetail::from($_POST['update']);
     $order->status($_POST['status']);
+    $order->clerk($clerk);
     $order->update();
     $url = Router::toUrl("home/order/{$order->status()}/{$order->id()}");
     Router::redirect($url);
--- controllers/clerk/order_detail.php
+++ controllers/clerk/order_detail.php
@@ -1,10 +1,11 @@
 <?php
+require_once 'models/clerk/Clerk.php';
+require_once 'models/shopbook/ShopBook.php';
 require_once 'models/studentorder/StudentOrder.php';
 require_once 'models/studentorderdetail/StudentOrderDetail.php';
-
 $id = Router::resource(2);
 $order = studentorder::from($id);
-$clerk = ($order->clerk() == NULL) ? '' : $order->clerk()->id();
+$clerks = Clerk::find();
 $orderdetails = StudentOrderDetail::find($order);
 $save_valu = $order->status();
 $total = 0;
@@ -13,4 +14,14 @@ foreach( $orderdetails as $orderdetail )
     $price = $orderdetail->book()->price();
     $number = $orderdetail->number();
     $total += ($price * $number);
+}
+
+$shopbook = [];
+foreach ($orderdetails as $k => $orderdetail) {
+    $shopbook[$k] = ShopBook::from($orderdetail->book());
+}
+
+$clerkArgs =[];
+foreach ($clerks as $clerk ) {
+    $clerkArgs[$clerk->id()] = $clerk->name();
 }
