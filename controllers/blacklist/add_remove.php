0b7a31ae6ff56b5672912c2f247672ea710873b1
esbb48 <4j1k3>
2013-11-03 00:52:03 +0800

    Fix blacklist bug for isset

--- controllers/blacklist/add_remove.php
+++ controllers/blacklist/add_remove.php
@@ -4,10 +4,10 @@ require_once 'models/user/User.php';
 
 $blacklist = Blacklist::from(User::from($_SESSION['user_id']));
 
-if(isset($_POST['add_user'])) {
+if(!empty($_POST['add_user'])) {
     $blacklist->add(user::find($_POST['add_user']));
 }
-if(isset($_POST['remove_user'])) {
+if(!empty($_POST['remove_user'])) {
     $blacklist->remove(user::find($_POST['remove_user']));
 }
 $url = Router::toUrl("message/blacklist");
